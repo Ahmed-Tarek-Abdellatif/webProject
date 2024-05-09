4b646446c15da9d55541a0fd472e6795fb775e4a
khaled moahmed <khaledmohamed9757@gmail.com>
2024-05-10 00:59:43 +0300

    add dbconn

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dbconn',function(){
+    return view('dbconn');
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
